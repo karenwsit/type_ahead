b7569b5254218d89423cea86d22d9e6a2602ed25
karenwsit <karenwsit@gmail.com>
2016-04-24 19:26:37 -0700

    added unidecode to address unicode problems for apostrophes

--- type_ahead.py
+++ type_ahead.py
@@ -1,5 +1,5 @@
 import sys
-# from pytrie import SortedStringTrie as trie
+from unidecode import unidecode
 
 class Item(object):
     def __init__(self, item_type, item_id, score, words):
@@ -13,42 +13,53 @@ class Node(object):
         self.children = {}
         self.item_ids = set()
 
-    def add(self, item, trie, total_items):
+    def add(self, item, trie):
 
-        current_node = None  #pointer
+        current_node = None  # pointer
 
-        for word in item.words:  #['adam', "d'angelo"]
+        for word in item.words:
+            clean_word = unidecode(word.decode('utf8'))
+            print clean_word
             current_node = trie
-            for letter in word:
+            for letter in clean_word:
                 if letter not in current_node.children:
+                    # print "CREATING NEW NODE"
                     new_node = Node()
                     current_node.children[letter] = new_node
-                    # print "current_node.children", current_node.children
-                    # print "WHYYYYYYY"
                     new_node.item_ids.add(item.id)
                     current_node = new_node
+                    # print "current_node.children", current_node.children
+                    # print "current_node.item_ids", current_node.item_ids
                 else:
+                    # print "NO NEW NODE, KEEP TRAVERSING"
                     current_node = current_node.children[letter]
                     current_node.item_ids.add(item.id)
-                #     print "NOOOO"
-                # print 'trie children:', trie.children
-                # print 'trie item_ids:', trie.item_ids
+                    # print "current_node.children", current_node.children
+                    # print "current_node.item_ids", current_node.item_ids
+
+    def delete(self, item_id, trie):
+        current_node = trie        
 
 
 class TypeAhead(object):
     def add(self, item, trie, total_items):
         total_items[item.id] = item
         trie.add(item, trie)
+        print "TOTAL ITEMS:", total_items
+        print "FIRST LEVEL:", trie.children
+        print "FIRST LEVEL ITEMS:", trie.item_ids
 
-    def delete(self, item_id):
+    def delete(self, item_id, trie, total_items):
+        total_items.pop(item_id, None)  # remove from total_items dictionary
+        trie.delete(item_id, trie)
 
-    def query(self, res_num, query):
+    # def query(self, res_num, query):
 
-    def wquery(self, res_num, boost_num, )
+    # def wquery(self, res_num, boost_num, )
 
 
 def read_stdin():
-    trie = Node()    #root of the trie
+    trie = Node()    # root of the trie
     total_items = {}     # keeps track of all items
     type_ahead = TypeAhead()
 
@@ -58,9 +69,10 @@ def read_stdin():
 
         if line[0] == 'ADD':
             item = Item(line[1], line[2], float(line[3]), [word.lower() for word in line[4:]])
-            TypeAhead.add(item, trie, total_items)
+            type_ahead.add(item, trie, total_items)
 
-        # if line[0] == 'DEL':
+        if line[0] == 'DEL':
+            type_ahead.delete(line[1], trie, total_items)
 
         # if line[0] == 'QUERY':
 
