3b180ca3eb524db241cd083f5142d0c7de04f3fd
karenwsit <karenwsit@gmail.com>
2016-04-28 16:18:00 -0700

    added wquery function with new_score variable bug to be fixed

--- type_ahead.py
+++ type_ahead.py
@@ -10,7 +10,14 @@ class Item(object):
         self.score = score
         self.words = words
 
-    # def boost_score(self,)
+    def boost_score(self, boost_list):
+        # boost_list = [[type, 9:99], [id, 8.02], ...]
+        original_score = self.score
+        new_score = None
+        for boost in boost_list:
+            if self.type == boost[0] or self.id == boost[0]:
+                new_score = original_score * float(boost[1])
+        return new_score
 
 class Node(object):
     def __init__(self, children=None, items=None):
@@ -91,25 +98,28 @@ class TypeAhead(object):
         if item is None:
             print 'Error: Invalid Item ID'
 
-    def query(self, res_num, trie, total_items, query_list):
+    def query(self, res_num, trie, total_items, query_list, boost_list=None):
         query_items = trie.query(query_list[0], trie)
-        if query_items != None:
+        if query_items is not None:
             for i in range(1, len(query_list)):
                 new_query_items = trie.query(query_list[i], trie)
-                if new_query_items != None:
+                if new_query_items is not None:
                     query_items &= new_query_items  # &= set intersection update
-                else:  #no match
+                else:
+                    # no match
                     query_items = None
 
-        if query_items == None or res_num == 0:
+        #printing
+        if query_items is None or res_num == 0:
             print ""
+        elif boost_list is not None:
+            boosts_sorted_query_items = sorted(query_items, key=lambda item: (item.boost_score(boost_list), item.creation_id), reverse=True)
+            print " ".join([item.id for item in boosts_sorted_query_items[:res_num]])
         else:
             # print "QUERY_ITEMS:", query_items
             sorted_query_items = sorted(query_items, key=attrgetter('score', 'creation_id'), reverse=True)
             # print "SORTED_QUERY_ITEMS:", sorted_query_items
             print " ".join([item.id for item in sorted_query_items[:res_num]])
-
-    # def wquery(self, res_num, boost_num, ):
 
 
 def read_stdin():
@@ -134,10 +144,9 @@ def read_stdin():
         if line[0] == 'QUERY':
             type_ahead.query(int(line[1]), trie, total_items, [word.lower() for word in line[2:]])
 
-        # if line[0] == 'WQUERY':
-        #     for word in line:
-        #         if word.contains(":")
-        #     type_ahead.wquery(int(line[1]), line[2], )
+        if line[0] == 'WQUERY':
+            num_boosts = int(line[2])
+            type_ahead.query(int(line[1]), trie, total_items, [word.lower() for word in line[3+num_boosts:]], [boost.split(':') for boost in line[3:3+num_boosts]])
 
 if __name__ == '__main__':
     read_stdin()
