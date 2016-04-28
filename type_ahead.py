ca29db9d780e8d5e01e392a4f1b96d43b79f7fb6
karenwsit <karenwsit@gmail.com>
2016-04-27 18:05:09 -0700

    added query method in node class. bugs for none type to be debugged

--- type_ahead.py
+++ type_ahead.py
@@ -2,11 +2,14 @@ import sys
 from unidecode import unidecode
 
 class Item(object):
-    def __init__(self, item_type, item_id, score, words):
+    def __init__(self, item_type, item_id, creation_id, score, words):
         self.type = item_type
         self.id = item_id
+        self.creation_id = creation_id
         self.score = score
         self.words = words
+
+    # def boost_score(self,)
 
 class Node(object):
     def __init__(self, children=None, items=None):
@@ -14,7 +17,6 @@ class Node(object):
         self.items = set()
 
     def add(self, item, trie):
-
         current_node = None  # pointer
 
         for word in item.words:
@@ -38,7 +40,6 @@ class Node(object):
                     # print "current_node.items", current_node.items
 
     def delete(self, item, trie):
-
         current_node = None   # pointer
 
         for word in item.words:
@@ -46,9 +47,9 @@ class Node(object):
             clean_word = unidecode(lower_word.decode('utf8'))
             print clean_word
             current_node = trie
-            for letter in clean_word:  #[adam, black]
+            for letter in clean_word:  # [adam, black]
                 if letter not in current_node.children:
-                    print "Error: Invalid Item Id"
+                    print "Error: Letter not found"
                 else:
                     current_node = current_node.children[letter]
                     print "BEFORE: current_node.items", current_node.items
@@ -56,7 +57,25 @@ class Node(object):
                     print "AFTER: current_node.items", current_node.items
                     print "CHILDREN:", current_node.children
 
+    def query(self, query_list, trie):
+        current_node = None  # pointer
+
+        if query_list:
+            for word in query_list:
+                clean_word = unidecode(word.decode('utf8'))
+                current_node = trie
+                for letter in clean_word:
+                    if letter in current_node.children:
+                        current_node = current_node.children[letter]
+                    else:
+                        return None
+                query_ids = current_node.items.copy()  # should return the last node with all the items that each word matches
+            return query_ids
+        else:
+            return None
+
 class TypeAhead(object):
+
     def add(self, item, trie, total_items):
         total_items[item.id] = item
         trie.add(item, trie)
@@ -66,36 +85,50 @@ class TypeAhead(object):
 
     def delete(self, item_id, trie, total_items):
         item = total_items.pop(item_id, None)  # remove from total_items dictionary
-        trie.delete(item, trie)
-        print "TOTAL ITEMS:", total_items
-        print "FIRST LEVEL:", trie.children
-        print "FIRST LEVEL ITEMS:", trie.items
+        if item:
+            trie.delete(item, trie)
+            print "TOTAL ITEMS:", total_items
+            print "FIRST LEVEL:", trie.children
+            print "FIRST LEVEL ITEMS:", trie.items
+        if item is None:
+            print 'Error: Invalid Item ID'
 
-    # def query(self, res_num, query):
+    def query(self, res_num, trie, query_list):
+        query_ids = trie.query(query_list[:1], trie)
+        if query_ids and len(query_list) > 1:
+            query_ids &= trie.query(query_list[1:], trie)  # &= set intersection update
 
-    # def wquery(self, res_num, boost_num, )
+        print "QUERY IDS:", query_ids
+
+    # def wquery(self, res_num, boost_num, ):
 
 
 def read_stdin():
     trie = Node()    # root of the trie
     total_items = {}     # keeps track of all items
     type_ahead = TypeAhead()
+    creation_id = 1
 
     #returns an iterator object; if value called == sentinel, StopIteration will be raised; ignores the first command, the int
     for raw_line in iter(sys.stdin.readline, ""):
         line = raw_line.strip().split()
+        print line
 
         if line[0] == 'ADD':
-            item = Item(line[1], line[2], float(line[3]), [word.lower() for word in line[4:]])
+            creation_id += 1
+            item = Item(line[1], line[2], creation_id, float(line[3]), [word.lower() for word in line[4:]])
             type_ahead.add(item, trie, total_items)
 
         if line[0] == 'DEL':
             type_ahead.delete(line[1], trie, total_items)
 
-        # if line[0] == 'QUERY':
+        if line[0] == 'QUERY':
+            type_ahead.query(int(line[1]), trie, [word.lower() for word in line[2:]])
 
         # if line[0] == 'WQUERY':
+        #     for word in line:
+        #         if word.contains(":")
+        #     type_ahead.wquery(int(line[1]), line[2], )
 
 if __name__ == '__main__':
     read_stdin()
-
